9bb9f3d3702436e6ed95730997ce90cf7539e7e4
Matthew Bayers <matthew.bayers@yahoo.com>
2026-09-23 13:16:15 -0700

    MongoDB update, adding in seed data, to DB

---
+++ bin/seedTrips.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const mongoose = require('mongoose');
+
+require('../app_server/models/db');
+const Trip = require('../app_server/models/travlr');
+
+const trips = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '../data/trips.json'), 'utf8')
+);
+
+const seedTrips = async () => {
+  await mongoose.connection.asPromise();
+  await Trip.deleteMany({});
+  await Trip.insertMany(trips);
+  console.log(`Seeded ${trips.length} trips`);
+  await mongoose.connection.close();
+};
+
+seedTrips().catch(async err => {
+  console.error(err);
+  await mongoose.connection.close();
+  process.exit(1);
+});
